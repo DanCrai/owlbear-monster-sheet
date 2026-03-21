6dc8c506565f7270b2ba059638f00f0331150eac
darkdtq darkdtq <rarescrai@yahoo.com>
2026-03-21 12:40:41 +0200

    new context menu with icon

--- scripts/contextMenu.js
+++ scripts/contextMenu.js
@@ -2,19 +2,33 @@ import { attachMonsterToToken } from "./token.js";
 import { getMonsterList } from "./storage.js";
 
 export function setupContextMenu() {
+    console.log("Setuping context menu!");
     OBR.contextMenu.create({
         id: "monster-menu",
-        icons: [],
-        onClick: async (context) => {
-            const monsters = await getMonsterList();
 
+        icons: [
+            {
+                icon: "https://dancrai.github.io/owlbear-monster-sheet/context-menu-monster-icon.svg",
+                label: "Attach Monster",
+                filter: {
+                    every: [
+                        { key: "layer", value: "CHARACTER" }
+                    ]
+                }
+            }
+        ],
+
+        onClick: async (context) => {
+            console.log("awaiting monster!");
+            const monsters = await getMonsterList();
+            console.log("got monster");
             const choice = prompt(
                 "Attach monster:\n" + monsters.join("\n")
             );
 
             if (!choice) return;
 
-            attachMonsterToToken(context.items, choice);
+            await attachMonsterToToken(context.items, choice);
         }
     });
 }
